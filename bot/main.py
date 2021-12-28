0264b43094b10d75b34d64348b2438bbdf661055
fauxes <96756458+fauxes@users.noreply.github.com>
2021-12-28 03:47:24 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-TOKEN = "OTI1MTM1MTA1MzIyNzc4Njk2.YcotNQ.VWgLYyfTNMvqDlugdm0My7nrsIM"
+TOKEN = "OTI1MTM1MTA1MzIyNzc4Njk2.YcotNQ.ZoQtlwZHMArPswkroRkgDeUl4Pc"
 BOT = Bot(command_prefix='alpha')
 
-
-BOT.run(TOKEN)
+BOT.run(os.getenv('DISCORD.TOKEN'))
