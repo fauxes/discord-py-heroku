deca0b5d7f7fd1065bce480b34e8c069d3813c83
fauxes <96756458+fauxes@users.noreply.github.com>
2021-12-28 04:25:12 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,4 +4,8 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 BOT = Bot(command_prefix='alpha')
 
+@BOT.event
+async def on_ready():
+    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the world burn"))
+    
 BOT.run(os.getenv('DISCORD_TOKEN'))
