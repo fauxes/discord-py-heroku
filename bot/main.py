0ad0ea5c93861e8d4d55d55149a8fdf1610053a8
fauxes <96756458+fauxes@users.noreply.github.com>
2022-02-28 19:47:58 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,6 @@ BOT = Bot(command_prefix='alpha')
 
 @BOT.event
 async def on_ready():
-    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Donate to Ukraine!"))
-    await BOT.change_presence(activity = discord.Game("Donate to help Ukraine crisis! https://donate.redcross.org.uk/appeal/ukraine-crisis-appeal")) 
+    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Donate to Ukraine! More in bio."))
     
 BOT.run(os.getenv('DISCORD_TOKEN'))
