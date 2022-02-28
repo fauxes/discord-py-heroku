2e1985bb304b1048ecf89da7301ab604ed7e82d4
fauxes <96756458+fauxes@users.noreply.github.com>
2022-02-28 12:41:51 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,6 @@ BOT = Bot(command_prefix='alpha')
 
 @BOT.event
 async def on_ready():
-    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the world burn"))
+    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Donate to help Ukraine crisis! https://donate.redcross.org.uk/appeal/ukraine-crisis-appeal"))
     
 BOT.run(os.getenv('DISCORD_TOKEN'))
