ed2699ec8fa4d59a0e0f33c30cfe9b39148229b7
fauxes <96756458+fauxes@users.noreply.github.com>
2022-07-11 21:35:45 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,13 @@
 import os
+
+import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+BOT = Bot(command_prefix='alpha')
 
-@bot.event
+@BOT.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
-if __name__ == "__main__":
-    bot.run(TOKEN)
+    await BOT.change_presence(activity=discord.Game(name="Cyberpunk 2077"))
+    
+BOT.run(os.getenv('DISCORDTOKEN'))
