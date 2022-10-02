982f90c1e991f99b5cf612e524caa26300228d26
fauxes <96756458+fauxes@users.noreply.github.com>
2022-10-02 23:27:21 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,6 +8,6 @@ BOT = Bot(command_prefix='alpha')
 
 @BOT.event
 async def on_ready():
-    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="WarNymph radio"))
+    await BOT.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Grimescord playlist"))
     
 BOT.run(os.getenv('DISCORD_TOKEN'))
